39e5d894394a20f12b1f1b9725b431f6892d6c04
Juansanchezrodriguez1999 <juansanchezrodriguez1999@gmail.com>
2023-05-30 12:54:04 +0200

    refactor heatmap

--- utils.py
+++ utils.py
@@ -7,28 +7,6 @@ from folium.plugins import (Fullscreen, LocateControl, MeasureControl,
                             MousePosition)
 
 
-def generate_map(location, zoom) -> folium.Map:
-    map_folium = folium.Map(location=location, zoom_start=zoom)
-
-    Fullscreen().add_to(map_folium)
-    LocateControl().add_to(map_folium)
-    MousePosition().add_to(map_folium)
-    MeasureControl("bottomleft").add_to(map_folium)
-    map_layers_dict = {
-        "World Street Map": "https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}",
-        "Satellite": "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
-        "Google Maps": "https://mt1.google.com/vt/lyrs=m&x={x}&y={y}&z={z}",
-        "Google Satellite": "https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}",
-        "Google Terrain": "https://mt1.google.com/vt/lyrs=p&x={x}&y={y}&z={z}",
-        "Google Satellite Hybrid": "https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}",
-    }
-    for layer in map_layers_dict:
-        folium.TileLayer(
-            tiles=map_layers_dict[layer], attr=layer, name=layer, show=False
-        ).add_to(map_folium)
-
-    folium.LayerControl(position="bottomleft").add_to(map_folium)
-    return map_folium
 
 
 def popup_html_from_df(df, row, vega_chart):
@@ -99,78 +77,27 @@ def generate_table_from_dict(dict_sample):
     return table_list
 
 
-def popup_html_from_mongo(sample, pics):
-    # print(sample)
-    sample.pop("Pictures")
-
-    keys = list(sample.keys())
-    half = int(len(keys) / 2)
-
-    first_half = {k: sample[k] for k in list(sample)[:half]}
-    last_half = {k: sample[k] for k in list(sample)[half:]}
-
-    first_table = generate_table_from_dict(first_half)
-    last_table = generate_table_from_dict(last_half)
-    keys = list(sample.keys())
+def popup_html_from_mongo(sample):
+    combined_table = generate_table_from_dict(sample)
     html_list = []
 
-    # Add tag to open and close each table
-    first_table.insert(0, "<table border='1' class='dataframe'> <tbody>")
-    first_table.append("</tbody> </table>")
+    # Add tag to open and close the table
+    combined_table.insert(0, "<table border='1' class='dataframe'> <tbody>")
+    combined_table.append("</tbody> </table>")
 
-    last_table.insert(0, "<table border='1' class='dataframe'> <tbody>")
-    last_table.append("</tbody> </table>")
-
-    last_table = "".join(last_table)
-    first_table = "".join(first_table)
+    combined_table = "".join(combined_table)
 
     html_table_div = f"""
-    <table style="border-collapse: collapse; width: 100%;" border="1">
+    <table style="width: 100%;">
     <tbody>
     <tr>
-        <td style="width: 50%;">{first_table}</td>
-        <td style="width: 50%;">{last_table}</td>
+        <td style="padding: 10px;">{combined_table}</td>
     </tr>
     </tbody>
     </table>
- 
-    
     """
+
     html_list.append(html_table_div)
-
-    html_species = f"""
-    <table style= "width: 100%; margin-top: 10px" class='dataframe' border='1'>
-        <caption style="background-color:#3630a3; 
-                  color:white; 
-                  padding:5px;">Species</caption>
-        <tr>
-            <th> Name </th>
-            <th> Ind </span></th>
-        </tr>
-    
-    """
-    html_list.append(html_species)
-    sample["Species"]
-    species_sorted = sorted(sample["Species"], key=lambda d: d["Name"])
-
-    for sp in range(len(sample["Species"])):
-        html_species_row = (
-            """
-            <tr>
-                <td> {}</td>""".format(
-                species_sorted[sp]["Name"]
-            )
-            + """
-                <td> {} </td>""".format(
-                species_sorted[sp]["Ind"]
-            )
-            + """
-            </tr>"""
-        )
-        html_list.append(html_species_row)
-
-    html_list.append("</table>")
-    html_list.append(pics)
     html_def = "".join(html_list)
 
     return html_def
