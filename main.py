853545edc3d62547064d27aeb3f81ca2bbc069d5
Juansanchezrodriguez1999 <juansanchezrodriguez1999@gmail.com>
2023-05-31 09:47:01 +0200

    update requirements and refactor code

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ st.title("Flora Visualization")
 
 "## Start Exploring"
 """
-- `Flora`: You will be able to view the data from the Flora database. You can choose to display all samples or apply filters by specifying 
-    only the fields of interest. After selecting the samples, they will be displayed on the map along with relevant information obtained from the database. 
+- `Flora`: You will be able to view the data from the Flora database. You can apply filters by specifying only the fields of interest. 
+    After selecting the samples, they will be displayed on the map along with relevant information obtained from the database. 
     This information may include the species present in that location, the lithology, or the natural park to which the sample belongs, among others. 
     Additionally, any images associated with the sample will also be displayed.     
 
