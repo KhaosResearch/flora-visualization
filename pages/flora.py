5aa47af488da4280de4987a3922413a077b8b346
Juansanchezrodriguez1999 <juansanchezrodriguez1999@gmail.com>
2023-06-29 13:51:00 +0200

    Add pictures in table and change empty values

--- pages/flora.py
+++ pages/flora.py
@@ -1,16 +1,14 @@
-import base64
 import datetime
+import statistics as stats
+
 import folium
 import streamlit as st
-from streamlit_folium import folium_static
-
 from config import settings
-import statistics as stats
-from folium.plugins import MarkerCluster,HeatMap
-
+from folium.plugins import HeatMap, MarkerCluster
 from minio_client import minio_connection
 from mongo_client import get_collection_object
-from utils import (popup_html_from_mongo)
+from streamlit_folium import folium_static
+from utils import popup_html_from_mongo
 
 # Set the page configuration
 st.set_page_config(layout="wide")
@@ -62,16 +60,14 @@ with st.sidebar.form(key="my_form"):
     natural_site_uniques = collection.distinct(
         "NaturalSite", {"NaturalSite": {"$ne": None}}
     )
-    natural_site = st.multiselect(
-        "Natural Site", natural_site_uniques, help="Optional"
-    )
+    natural_site = st.multiselect("Natural Site", natural_site_uniques, help="Optional")
 
-    species_unique = collection.distinct(
-        "specieName", {"specieName": {"$ne": None}}
-    )
+    species_unique = collection.distinct("specieName", {"specieName": {"$ne": None}})
     species = st.multiselect("Species name", species_unique, help="Optional")
 
-    recorded_by_unique = collection.distinct("recordedBy", {"recordedBy": {"$ne": None}})
+    recorded_by_unique = collection.distinct(
+        "recordedBy", {"recordedBy": {"$ne": None}}
+    )
     recorded_by = st.multiselect("Recorded by", recorded_by_unique, help="Optional")
 
     submit_button = st.form_submit_button(label="Search", help="Submit Button")
@@ -98,12 +94,7 @@ if institution:
         if len(particular_institution) > 0:
             institution_empty = False
             institution_exp.append(
-                {
-                    "institutionCode": {
-                        "$regex": particular_institution,
-                        "$options": "i",
-                    }
-                }
+                {"institutionCode": {"$regex": particular_institution, "$options": "i"}}
             )
     if len(institution_exp) > 1:
         query_list.append({"$or": institution_exp})
@@ -143,18 +134,12 @@ if natural_site:
         if len(particular_natural_site) > 0:
             natural_site_empty = False
             natural_site_exp.append(
-                {
-                    "NaturalSite": {
-                        "$regex": particular_natural_site,
-                        "$options": "i",
-                    }
-                }
+                {"NaturalSite": {"$regex": particular_natural_site, "$options": "i"}}
             )
     if len(natural_site_exp) > 1:
         query_list.append({"$or": natural_site_exp})
     elif len(natural_site_exp) == 1:
         query_list.append({"$or": natural_site_exp})
-
 
 
 try:
@@ -173,11 +158,15 @@ latitude_location = []
 longitude_location = []
 if len(query_list) > 1:
     for document in cursor_list:
-        if(document["decimalLatitude"]):
-            latitude_location.append(float(document["decimalLatitude"].replace(",",".")))
-            longitude_location.append(float(document["decimalLongitude"].replace(",",".")))   
-        
-    if(latitude_location):
+        if document["decimalLatitude"]:
+            latitude_location.append(
+                float(document["decimalLatitude"].replace(",", "."))
+            )
+            longitude_location.append(
+                float(document["decimalLongitude"].replace(",", "."))
+            )
+
+    if latitude_location:
         median_latitude = stats.median(latitude_location)
         median_longitude = stats.median(longitude_location)
     else:
@@ -196,10 +185,11 @@ flora_map = folium.Map(
     width="100%",
     height="100%",
     tiles="https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}",
-    attr="Google Satellite",    )
+    attr="Google Satellite",
+)
 
 # Create a marker cluster layer
-marker_cluster = MarkerCluster(name='Marcadores', show=True).add_to(flora_map)
+marker_cluster = MarkerCluster(name="Marcadores", show=True).add_to(flora_map)
 
 # For each matching document, create a marker on the map with a popup containing the document's images
 long_lat = []
@@ -210,18 +200,27 @@ heatmap_long_lat = []
 
 if submit_button:  # or all_samples
     for document in cursor_list:
-        html_list = []
-        long_lat.append([float(document["decimalLatitude"].replace(",",".")),float(document["decimalLongitude"].replace(",","."))])
-        # There are some sample without longitude-latitude
-        if document["decimalLatitude"] and document["decimalLongitude"]:
-            heatmap_long_lat.append([float(document["decimalLatitude"].replace(",",".")),float(document["decimalLongitude"].replace(",","."))])
-            
+        pictures_list_link = []
+        if "Pictures" in document:
+            num_pics = len(document["Pictures"])
+            for i in range(num_pics):
+                # Get the image from Minio
+                minio_image = client.presigned_get_object(
+                    settings.MINIO_BUCKET_FLORA,
+                    document["Pictures"][i].replace("enbic2lab/", ""),
+                )
+                pictures_list_link.append(minio_image)
+        long_lat.append([float(document["decimalLatitude"].replace(",", ".")), float(document["decimalLongitude"].replace(",", "."))])
+        # There are some sample without longitude-latitude      
+        if float(document["decimalLatitude"].replace(",", ".")) and float(document["decimalLongitude"].replace(",", ".")):
+            heatmap_long_lat.append([float(document["decimalLatitude"].replace(",", ".")), float(document["decimalLongitude"].replace(",", "."))])
+            document["Pictures"] = pictures_list_link
             # Create the HTML code to display the table
             html = popup_html_from_mongo(document)
             iframe = folium.IFrame(html, width=650, height=650)
-            popup = folium.Popup(iframe)
+            popup = folium.Popup(iframe, lazy=True)
             marker = folium.Marker(
-                [float(document["decimalLatitude"].replace(",",".")),float(document["decimalLongitude"].replace(",","."))],
+                [float(document["decimalLatitude"].replace(",", ".")), float(document["decimalLongitude"].replace(",", "."))],
                 popup=popup,
                 tooltip=document["specieName"],
             )
@@ -229,7 +228,7 @@ if submit_button:  # or all_samples
 
 
 # Create a HeatMap layer with the heatmap data
-heatmap_layer = HeatMap(heatmap_long_lat, name='HeatMap', show=True)
+heatmap_layer = HeatMap(heatmap_long_lat, name="HeatMap", show=True)
 
 # Add the heatmap layer and marker layer to the map
 heatmap_layer.add_to(flora_map)
